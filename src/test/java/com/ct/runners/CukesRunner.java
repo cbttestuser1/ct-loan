3f1bb532d3148efc3277b1abf5cc7fdf2018cf48
abujhon <abujhon@outlook.com>
2017-12-15 21:48:59 -0500

    Set up the basic framwork. trying to connect the drivers.

--- src/test/java/com/ct/runners/CukesRunner.java
+++ src/test/java/com/ct/runners/CukesRunner.java
@@ -1,5 +1,18 @@
 package com.ct.runners;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "resources/features", 
+        tags = "@Smoke-Test", 
+        glue = "com.ct.stepDefinitions", 
+        plugin = {"html:target/cucumber-reports", "json:target/cucumber.json" }, 
+        dryRun = true)
+
 public class CukesRunner {
 
 }
