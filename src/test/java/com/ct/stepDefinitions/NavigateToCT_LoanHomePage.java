f8921b76fb87c8ac1c0d0cd4668786d5b06dc8eb
abujhon <abujhon@outlook.com>
2017-12-15 23:23:45 -0500

    pushing a change

--- src/test/java/com/ct/stepDefinitions/NavigateToCT_LoanHomePage.java
+++ src/test/java/com/ct/stepDefinitions/NavigateToCT_LoanHomePage.java
@@ -13,15 +13,15 @@ import cucumber.api.java.en.When;
 public class NavigateToCT_LoanHomePage {
 
 	
-	
-	@Given("^I go to CT-Loan web page$")
-	public void i_go_to_CT_Loan_web_page() throws Throwable {
-	   
+	@Given("^I am on the home page of \"([^\"]*)\"$")
+	public void i_am_on_the_home_page_of(String arg1) throws Throwable {
+		Driver.getInstance().get(CTContstants.URL);
 	}
 
-	@When("^I navigate to \"([^\"]*)\"$")
-	public void i_navigate_to(String arg1) throws Throwable {
-		Driver.getInstance().get(CTContstants.URL);	   
+
+	@When("^I am on the home page$")
+	public void i_am_on_the_home_page() throws Throwable {
+	   
 	}
 
 	@Then("^I should be able to see text CT-Loan$")
--- src/test/resources/features/navigateToCT-LoanHomepage.feature
+++ src/test/resources/features/navigateToCT-LoanHomepage.feature
@@ -2,6 +2,6 @@ Feature: navigate to CT-Loan home page
 
   @SmokeTest
   Scenario: check the system up and running
-    Given I set up the URL
-    When I navigate to "http://cybertekschool.com/ctl.dev"
+    Given I am on the home page of "http://cybertekschool.com/ctl.dev"
+    When I am on the home page
     Then I should be able to see text CT-Loan
