b4b5d0e1892ffe11c796de416609a774799f5857
ruslK <russkasymov@gmail.com>
2017-12-17 00:48:21 -0500

    Ticket CTL-68, for creating Page Object Model, Regisration Page 4, done.

---
+++ src/test/java/com/ct/pages/CT_Loan_Reg_P4_POM.java
@@ -0,0 +1,223 @@
+package com.ct.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com.ct.utilities.Driver;
+
+public class CT_Loan_Reg_P4_POM {
+
+	public CT_Loan_Reg_P4_POM() {
+		PageFactory.initElements(Driver.getInstance(), this);
+	}
+
+	// Element for Check "Financial Information" Locator, text
+	public static final String expFinanInfoText = "Financial Information";
+	@FindBy(xpath = "//*[@id='Secti.on3-li']//h2")
+	public static WebElement acctualFinancialInfoText;
+
+	/*
+	 * Element for question: 'Is this your first application to CT Loan?'
+	 */
+	// Element for question text
+	public static final String expFistApplicaionCTLoanText = "Is this your first application to CT Loan?";
+	@FindBy(xpath = "//*[@id='Radio1-li']//label")
+	public static WebElement isItFisrtApplicationCTLoanText;
+
+	// Elements for "YES" and "NO" radio buttons, and method
+	// for select specific radio button
+	@FindBy(id = "Radio1_1")
+	public static WebElement firstCTLoan_YES;
+
+	@FindBy(id = "Radio1_2")
+	public static WebElement firstCTLoan_NO;
+
+	public static void selectFistCTLoan(String answer) {
+		switch (answer.toLowerCase()) {
+		case "yes":
+			firstCTLoan_YES.click();
+			break;
+		case "no":
+			firstCTLoan_NO.click();
+			break;
+		}
+	}
+
+	/*
+	 * Check text for "What is your annual income?" and input box Element for
+	 * this
+	 */
+
+	public static final String expAnnualIncomeText = "What is your annual income?";
+
+	@FindBy(css = "#SingleLine3-li .labelName")
+	public static WebElement annualIncomeText;
+
+	@FindBy(xpath = "//*[@name='SingleLine3']")
+	public static WebElement annualIncomeIputBox;
+
+	/*
+	 * Check text for "Income type" and element for Selection and method for
+	 * selection
+	 */
+
+	public static final String expIncomeTypeText = "Income type";
+	@FindBy(xpath = "//*[@id='Dropdown5-li']/*[@class = 'labelName']")
+	public static WebElement incomeTypeText;
+
+	// WebElement for 'Income Type' Selector
+	@FindBy(xpath = "//select[@name='Dropdown5']")
+	public static WebElement incomeTypeSelector;
+	private static Select incomeType = new Select(incomeTypeSelector);
+
+	public static enum IncomeTypeEnum {
+		Biweekly, Monthly, Yearly
+	}
+
+	// Method for working with 'Income Type' Selector
+	// selection by value with ENUM
+	public static void selectIncomeTypeByValue(IncomeTypeEnum type) {
+		switch (type) {
+		case Biweekly:
+			incomeType.selectByValue("Biweekly");
+			break;
+		case Monthly:
+			incomeType.selectByValue("Monthly");
+			break;
+		case Yearly:
+			incomeType.selectByValue("Yearly");
+			break;
+		}
+	}
+
+	/*
+	 * Check text for 'Do you own a house or rent?'* and radio buttons for this
+	 * question
+	 */
+
+	// Check text of question:
+	public static final String expOwnOrRentHouseText = "Do you own a house or rent?";
+	@FindBy(xpath = "//li[@id='Radio2-li']//label")
+	public static WebElement isOwnOrRentHouseText;
+
+	@FindBy(id = "Radio2_1")
+	public static WebElement OwnOrRentHouse_OWN;
+
+	@FindBy(id = "Radio2_2")
+	public static WebElement OwnOrRentHouse_RENT;
+
+	public static void selectOwnOrRentHouse(String answer) {
+		switch (answer.toLowerCase()) {
+		case "yes":
+			OwnOrRentHouse_OWN.click();
+			break;
+		case "no":
+			OwnOrRentHouse_RENT.click();
+			break;
+		}
+	}
+
+	/*
+	 * Check Text for question: 'How much do you spent in one month?' and input
+	 * box for text
+	 */
+
+	public static final String expSpendingInMonthText = "How much do you spent in one month?";
+
+	@FindBy(xpath = "//li[@id='Number3-li']//label")
+	public static WebElement SpendingInMonthText;
+
+	@FindBy(xpath = "//li[@id='Number3-li']//*[@name='Number3']")
+	public static WebElement SpendingInMonthInputBox;
+
+	/*
+	 * Check Text for question: 'How many dependents do you have?' and input box
+	 * for text
+	 */
+
+	public static final String expDependentsText = "How many dependents do you have?";
+
+	@FindBy(xpath = "//li[@id='Number4-li']//label")
+	public static WebElement DependentsText;
+
+	@FindBy(xpath = "//li[@id='Number4-li']//*[@name='Number4']")
+	public static WebElement DependentsInputBox;
+
+	/*
+	 * Check Text for question: 'How much savings do you have?' and input box
+	 * for text
+	 */
+
+	public static final String expSavingText = "How much savings do you have?";
+
+	@FindBy(xpath = "//li[@id='Number5-li']//label")
+	public static WebElement SavingText;
+
+	@FindBy(xpath = "//li[@id='Number5-li']//*[@name='Number5']")
+	public static WebElement SavingInputBox;
+
+	/*
+	 * Check text for "Desired Loan Term?" and element for Selection and method
+	 * for selection
+	 */
+
+	public static final String expDesiredLoanTermText = "Income type";
+	@FindBy(xpath = "//li[@id='Dropdown6-li']//label")
+	public static WebElement DesiredLoanTermText;
+
+	// WebElement for 'Income Type' Selector
+	@FindBy(css = ".Dropdown6")
+	public static WebElement DesiredLoanTermSelector;
+	private static Select DesiredLoanTerm = new Select(DesiredLoanTermSelector);
+
+	public static enum DesiredLoanType {
+		Months_12, Months_18, Months_24, Months_36
+	}
+
+	// Method for working with 'Desired Loan Term' Selector
+	// selection by value with ENUM
+	public static void selectDesiredLoanTermByValue(DesiredLoanType type) {
+		switch (type) {
+		case Months_12:
+			DesiredLoanTerm.selectByValue("12 months");
+			break;
+		case Months_18:
+			DesiredLoanTerm.selectByValue("18 months");
+			break;
+		case Months_24:
+			DesiredLoanTerm.selectByValue("24 months");
+			break;
+		case Months_36:
+			DesiredLoanTerm.selectByValue("36 months");
+			break;
+		}
+	}
+
+	/*
+	 * Check "Term Agreement" Text and CheckBox Element
+	 */
+
+	public static final String expTermAgreementText = "By checking this checkbox I confirm that all information I entered is correct and I agree to all terms and conditions.";
+
+	@FindBy(xpath = "//li[@id='DecisionBox-li']//label")
+	public static WebElement TermAgreementText;
+
+	@FindBy(css = "DecisionBox")
+	public static WebElement TermAgreemenCheckBox;
+
+	/*
+	 * Input box for 'Single Line"
+	 */
+
+	@FindBy(css = "#SingleLine5")
+	public static WebElement singleLineInputBox;
+
+	/*
+	 * "Submit" button
+	 */
+
+	@FindBy(css = ".fmSmtButton.submitColor.fmSmtButtom em")
+	public static WebElement submitButton;
+}
