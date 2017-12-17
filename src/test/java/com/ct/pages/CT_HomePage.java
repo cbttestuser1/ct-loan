758952568280081f05a7dbfcbf065fe8adfd9892
naji90 <naji90cr7@gmail.com>
2017-12-17 12:31:35 -0500

    new commit!!

--- src/test/java/com/ct/pages/CT_HomePage.java
+++ src/test/java/com/ct/pages/CT_HomePage.java
@@ -1,10 +1,11 @@
 package com.ct.pages;
 
 public class CT_HomePage {
-public static void main(String[] args) {
-	System.out.println("Test");
-	System.out.println("it is just check commit");
-	
-}
+	public static void main(String[] args) {
+		System.out.println("Test");
+		System.out.println("it is just check commit");
+		System.out.println("Test2");
+
+	}
 
 }
