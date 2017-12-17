2999b8680a792b907ed04a47babe0cce07b4a0b8
meerimkanatbek <meerimkanatbek@meerims-mbp.localdomain>
2017-12-17 14:12:48 -0500

    add step.def for CTL-20

---
+++ src/test/java/com/ct/stepDefinitions/CTL20_personal_info.java
@@ -0,0 +1,40 @@
+package com.ct.stepDefinitions;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CTL20_personal_info {
+	@Given("^I am on the home page of CT-Loan$")
+	public void i_am_on_the_home_page_of_CT_Loan() throws Throwable {
+	
+	    throw new PendingException();
+	}
+
+	@Then("^I add personal information data in CT-Loan personal info page$")
+	public void i_add_personal_information_data_in_CT_Loan_personal_info_page(DataTable arg1) throws Throwable {
+	   
+	    throw new PendingException();
+	}
+
+	@Then("^I choose a gender$")
+	public void i_choose_a_gender() throws Throwable {
+	  
+	    throw new PendingException();
+	}
+
+	@When("^I click on Next button$")
+	public void i_click_on_Next_button() throws Throwable {
+
+	    throw new PendingException();
+	}
+
+	@Then("^I have to end up in contact information page\\.$")
+	public void i_have_to_end_up_in_contact_information_page() throws Throwable {
+	
+	    throw new PendingException();
+	}
+
+}
