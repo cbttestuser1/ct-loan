77d16bcc0110bcf48bffa5ed7a753e9ebac7d589
Emilzeynalov <emilzeynal@gmil.com>
2017-12-18 10:28:51 -0500

    updated

--- src/test/java/com/ct/runners/CukesRunner.java
+++ src/test/java/com/ct/runners/CukesRunner.java
@@ -2,22 +2,17 @@ package com.ct.runners;
 
 import org.junit.runner.RunWith;
 
-   /*
-    * Please do not make changes to cucumber options without devUp's notice
-    * Please follow naming convention
-    */
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)   
-@CucumberOptions(
-		features = "./src/test/resources/features/", 
-        tags = "@SmokeTest", 
-        glue = "com.ct.stepDefinitions", 
-        plugin = {"html:target/cucumber-reports", "json:target/cucumber.json" }, 
-        dryRun = false
-        )
+/*
+ * Please do not make changes to cucumber options without devUp's notice
+ * Please follow naming convention
+ */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "./src/test/resources/features/", tags = "@SmokeTest", glue = "com.ct.stepDefinitions", plugin = {
+		"html:target/cucumber-reports", "json:target/cucumber.json" }, dryRun = false)
 
 public class CukesRunner {
 
