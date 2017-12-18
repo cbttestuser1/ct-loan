17d6820d5cfcb1ed841736eef4547ef5a9ad40b6
Dastanbek <dasti@dastans-mbp.fios-router.home>
2017-12-18 18:22:44 -0500

    updated

---
+++ src/test/java/com/ct/pages/CT_HomePage_1.java
@@ -0,0 +1,55 @@
+package com.ct.pages;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.ct.utilities.Driver;
+
+public class CT_HomePage_1 {
+	WebDriver driver;
+	
+	
+	public CT_HomePage_1(){
+		PageFactory.initElements(Driver.getInstance(), this);
+	}
+	@FindBy(css="input[name='SingleLine']")
+	public WebElement firstNam;
+	
+	@FindBy(css="input[name='SingleLine1']")
+	public WebElement middleNam;
+	
+	@FindBy(css="input[name='SingleLine2']")
+	public WebElement lastNam;
+	
+	@FindBy(id="Date-date")
+	public WebElement date;
+	
+	@FindBy(css="*[name='Number']")
+	public WebElement personalID;
+
+	
+	public void fillOutTheForm(String firstName, String middleName, String lastName, String dateOfBirth, String idNum ) {
+
+        firstNam.sendKeys(firstName);
+        middleNam.sendKeys(middleName);
+        lastNam.sendKeys(lastName);
+        date.sendKeys(dateOfBirth);
+        personalID.sendKeys(idNum);
+	}
+	
+        public void errorMSG(){
+		
+		String errorName=driver.findElement(By.id("error-SingleLine")).getText();
+		String errorMidName=driver.findElement(By.id("error-SingleLine1")).getText();
+		String errorLasName=driver.findElement(By.id("error-SingleLine2")).getText();
+		String errorDate=driver.findElement(By.id("error-Date")).getText();
+		String errorNumber=driver.findElement(By.id("error-Number")).getText(); 
+	
+	}
+}
+
+
--- src/test/java/com/ct/runners/CukesRunner.java
+++ src/test/java/com/ct/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)   
 @CucumberOptions(
 		features = "./src/test/resources/features/", 
-        tags = "@SmokeTest", 
-        glue = "com.ct.stepDefinitions", 
+        tags = "@Length", 
+        //glue = "com.ct.stepDefinitions", 
         plugin = {"html:target/cucumber-reports", "json:target/cucumber.json" }, 
         dryRun = false
         )
