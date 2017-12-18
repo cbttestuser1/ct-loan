555818c8a1308a3524d1b842bdb2affe93c6abe5
haghuseyin <haghuseyin@gmail.com>
2017-12-17 19:50:53 -0500

    first day

---
+++ src/test/java/com/ct/stepDefinitions/CTL_48_StepDefination.java
@@ -0,0 +1,19 @@
+package com.ct.stepDefinitions;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CTL_48_StepDefination {
+	
+	@When("^I do not fill the Contact Information fields$")
+	public void i_do_not_fill_the_Contact_Information_fields() throws Throwable {
+	   
+	}
+
+	@Then("^I should be able to see error messages for mandatory fields$")
+	public void i_should_be_able_to_see_error_messages_for_mandatory_fields() throws
+	 Throwable {
+	    
+	}
+
+}
--- src/test/resources/features/CTL-48_Test_Contact _Information_mandatory_fields.feature
+++ src/test/resources/features/CTL-48_Test_Contact _Information_mandatory_fields.feature
@@ -1,9 +1,10 @@
-@hgumus
+@CTL-48
 Feature: Test Contact Information mandatory fields 
 
 Scenario:
 Create and automateTest scenarios for the following fields in order to test mandatory/optional fields: 
 
+	#test1
 	Given I am on the home page of CT-Loan home page
 	And I filled the Personal Information with valid data
 	When I do not fill the Contact Information fields
