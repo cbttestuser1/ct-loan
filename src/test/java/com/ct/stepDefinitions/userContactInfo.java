348bcc365c80ce683519f11d0f99fab25a7005ff
zeinepmuratbekova <zeinepmuratbekova@zeineps-air.localdomain>
2017-12-18 18:44:23 -0500

    addition

---
+++ src/test/java/com/ct/stepDefinitions/userContactInfo.java
@@ -0,0 +1,43 @@
+package com.ct.stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class userContactInfo {
+	@Given("^User is on Contact Information Page$")
+	public void user_is_on_Contact_Information_Page() throws Throwable {
+		
+	    
+	}
+
+	@Given("^User enters Work Phone number$")
+	public void user_enters_Work_Phone_number() throws Throwable {
+	   
+	}
+
+	@Then("^Verify the Size:(\\d+) digits$")
+	public void verify_the_Size_digits(int arg1) throws Throwable {
+	   
+	}
+
+	@Then("^User enters Extension$")
+	public void user_enters_Extension() throws Throwable {
+	    
+	}
+
+	@Then("^User enters Email \\(work\\)$")
+	public void user_enters_Email_work() throws Throwable {
+	   
+	}
+
+	@Then("^Verify Size:(\\d+)-(\\d+) digits$")
+	public void verify_Size_digits(int arg1, int arg2) throws Throwable {
+	   
+	}
+
+	@Then("^User enters Personal \\(email\\)$")
+	public void user_enters_Personal_email() throws Throwable {
+	   
+	}
+
+}
