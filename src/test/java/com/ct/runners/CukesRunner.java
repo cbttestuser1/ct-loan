79956b103a970b25d058e735f34bf0db54dd9af6
zhanyl <zhanyl@zhanyls-air.localdomain>
2017-12-20 14:43:57 -0500

    fixing conflict on git

--- src/test/java/com/ct/runners/CukesRunner.java
+++ src/test/java/com/ct/runners/CukesRunner.java
@@ -2,7 +2,7 @@ package com.ct.runners;
 
 import org.junit.runner.RunWith;
 
-   /*
+/*
     * Please do not make changes to cucumber options without devUp's notice
     * Please follow naming convention
     */
@@ -10,14 +10,9 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)   
-@CucumberOptions(
-		features = "./src/test/resources/features/", 
-        tags = "@SmokeTest", 
-        glue = "com.ct.stepDefinitions", 
-        plugin = {"html:target/cucumber-reports", "json:target/cucumber.json" }, 
-        dryRun = false
-        )
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "./src/test/resources/features/", tags = "@page3", glue = "com.ct.stepDefinitions", plugin = {
+		"html:target/cucumber-reports", "json:target/cucumber.json" }, dryRun = false)
 
 public class CukesRunner {
 
