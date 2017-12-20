208e1d26004ebaa12f6669a1b1472fc9d60f307b
Beilul <beilulina13@gmail.com>
2017-12-19 23:13:29 -0500

    please lets not delete from the written codes

--- src/test/java/com/ct/pages/CT_HomePage.java
+++ src/test/java/com/ct/pages/CT_HomePage.java
@@ -1,5 +1,33 @@
 package com.ct.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.ct.utilities.Driver;
+
+
+
 public class CT_HomePage {
 
+	public CT_HomePage(){
+		
+		PageFactory.initElements(Driver.getInstance(), this);
+	}
+	
+	
+	@FindBy(css=".tempHeadContBdr")
+	public WebElement ct_Loan;
+	
+	
+	@FindBy(name="SingleLine")
+	public WebElement firstName;
+	
+	@FindBy(name="SingleLine1")
+	public WebElement middleName;
+	
+	@FindBy(name="SingleLine2")
+	public WebElement lastName;
+	
+	
 }
