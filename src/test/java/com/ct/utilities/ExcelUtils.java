c4a10c045abef2cc0f6b5d6a32599722a8d70a6e
Brad Pahari <bpahari21@gmail.com>
2017-12-21 16:47:45 -0500

    updated Higlight method

--- src/test/java/com/ct/utilities/ExcelUtils.java
+++ src/test/java/com/ct/utilities/ExcelUtils.java
@@ -1,12 +1,13 @@
 package com.ct.utilities;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 
 public class ExcelUtils {
 
---
+++ src/test/java/com/ct/utilities/HighLight.java
@@ -0,0 +1,14 @@
+package com.ct.utilities;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class HighLight {
+
+    public static void highlightElement(WebElement element, Object driver) throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].setAttribute('style', arguments[1);]", element, "border:solid red;");
+        Thread.sleep(1000);
+        js.executeScript("arguments[0].setAttribute('style',arguments[1]);", element, "");
+    }
+}
