89e1edf806b8e0ae4c2816946e71696d263b9f37
zeinepmuratbekova <zeinepmuratbekova@zeineps-air.localdomain>
2017-12-22 20:15:12 -0500

    done

--- src/test/java/com/ct/runners/CukesRunner.java
+++ src/test/java/com/ct/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)   
 @CucumberOptions(
 		features = "./src/test/resources/features/", 
-        tags = "@positiveInfoTest,@contact, @cantactInfo", 
+        tags = "@positiveInfoTest,@contact, @cantactInfo, @positiveEmp", 
         glue = "com.ct.stepDefinitions", 
         plugin = {"html:target/cucumber-reports", "json:target/cucumber.json" }, 
         dryRun = false
