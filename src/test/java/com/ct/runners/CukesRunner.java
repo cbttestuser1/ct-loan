bfb08bcb43f7cba0c16d6d51046826b4b79beae9
Bogdan Lutsenko <lutsenkob87@gmail.com>
2018-01-17 13:55:37 -0500

    commit

--- src/test/java/com/ct/runners/CukesRunner.java
+++ src/test/java/com/ct/runners/CukesRunner.java
@@ -3,6 +3,8 @@ package com.ct.runners;
 import java.io.File;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.ct.utilities.EmailStatus;
 import com.cucumber.listener.Reporter;
 
 /*
@@ -12,19 +14,20 @@ import com.cucumber.listener.Reporter;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-//comment
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "./src/test/resources/features/", tags = { "@Test112"}, glue = "com.ct.stepDefinitions", plugin = {
-		"html:target/cucumber-reports", "json:target/cucumber.json",
-		"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" }, monochrome = true, dryRun = false)
+@CucumberOptions(features = "./src/test/resources/features/", tags = {
+		"@Test112" }, glue = "com.ct.stepDefinitions", plugin = { "html:target/cucumber-reports",
+				"json:target/cucumber.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html" }, monochrome = true, dryRun = false)
 
 public class CukesRunner {
 	@AfterClass
-	public static void tearDown() {
+	public static void tearDown() throws Exception {
 		Reporter.loadXMLConfig(new File("./config.xml"));
 		Reporter.setSystemInfo("User", System.getProperty("user.name"));
 		Reporter.setSystemInfo("operation System", System.getProperty("os.name"));
 		Reporter.setTestRunnerOutput("Automated Test");
-		
+		EmailStatus.sendEmail("1danya0443@gmail.com", "lutsenkob87@gmail.com", "file:///Users/bogdanlutsenko/git/ct-loan/output/report.html");
 	}
 }
--- src/test/java/com/ct/utilities/EmailConfig.java
+++ src/test/java/com/ct/utilities/EmailConfig.java
@@ -1,11 +1,25 @@
 package com.ct.utilities;
 
+//public class EmailConfig {
+
+//	public static String server = "smtp.gmail.com";
+//	public static String from = "bobojon.rich@gmail.com";
+//	public static String password = "";
+//	public static String[] to = { "" };
+//	public static String subject = "Automation Report";
+//	public static String messageBody = "Automation Report";
+//	public static String attachmentName = "report.html";
+//}
+
+
+
 public class EmailConfig {
 	public static String server = "smtp.gmail.com";
-	public static String from = "bobojon.rich@gmail.com";
-	public static String password = "";
-	public static String[] to = { "" };
-	public static String subject = "Automation Report";
-	public static String messageBody = "Automation Report";
-	public static String attachmentName = "report.html";
+	public static String from = "1danya0443@gmail.com";
+	public static String password = "aa5583aa";
+	public static String[] to = {""};
+public static String subject = "Automation html report";
+public static String messageBody = "Automation Report";
+public static String attachmentName= "report.html";
+
 }
